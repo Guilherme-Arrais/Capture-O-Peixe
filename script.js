36a4def47da9af42471b4280933dcbbb93ea2a1d
Guilherme-Arrais <114041284+Guilherme-Arrais@users.noreply.github.com>
2022-09-27 21:30:34 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function fuga(x){
+   let t = Math.random()*4;//Intervalos entre números que podem ser sorteados
+   let sort = t.toFixed(0);
+   if(sort==1){}
+   else{
+   x.style.position="absolute";
+   x.style.left=Math.random()*800+"px";
+   x.style.top=Math.random()*300+"px";
+   }}
+ function clicar(){
+ let alerta =document.getElementById("alerta");
+ alerta.addEventListener(alert("VOCÊ CONSEGUIU !"));
+
+   }
